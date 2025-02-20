9d6af4624414e1d69780b861fd339ff489f5ed06
Yury Alencar <yuryalencar19@gmail.com>
2025-02-20 01:09:20 -0300

    feat: add more workers to run tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   reporter: [["line"], ["allure-playwright"]],
   use: {
     baseURL: 'http://127.0.0.1:3000',
